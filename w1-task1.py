a42bd2147588366e7e94dcb0c3b7b47984ad9745
Jackie Crick Wu <jackiecrickwu@gmail.com>
2023-09-01 11:12:02 +1000

    Task 1 done

--- w1-task1.py
+++ w1-task1.py
@@ -1,1 +1,13 @@
 # Demo
+import cv2 as cv
+
+def main():
+    kangaroo = cv.imread('/Users/jackiecrickwu/Desktop/kangaroo.jpeg')
+    cv.imshow('Kangaroo', kangaroo)
+    cv.waitKey(0)
+
+
+
+
+if  __name__ == '__main__':
+    main()
